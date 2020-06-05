6337192d309c8acc813beca69817c703048c2704
Davide Miola <davide.miola99@gmail.com>
2020-06-05 21:56:13 +0200

    Implemented exercise SW/part2/es04.py and tidied up SW/part2/es01.py a bit

--- SW/part2/es01.py
+++ SW/part2/es01.py
@@ -3,6 +3,7 @@ from EndPoint import EndPoint
 from Device import Device
 from User import User
 from Service import Service
+from typing import *
 
 import datetime
 import threading
@@ -11,58 +12,68 @@ import time
 import cherrypy
 import json
 
-# Thread class to manage timeouts for RESTCatalog
-class TimeoutManagerRunner(threading.Thread, cherrypy.process.plugins.SimplePlugin):
-    
-    # timeout is the number of seconds after the last subscription of a service or device to the Catalog such that they must be removed then
-    # period specifies (in seconds, too) the period to check for every device and service
-    def __init__(self, catalog, bus, timeout: float, period: float):
-        threading.Thread.__init__(self)
-        cherrypy.process.plugins.SimplePlugin.__init__(self, bus)
-        
-        self.catalog = catalog
-        self.timeout = timeout
-        self.period = period
-        
-        self.running = True
-
-    def run(self):
-        while self.running:
-            removed = False
-            
-            # Check all the devices
-            for device in list(self.catalog.database["devices"].values()):
-                timestamp = datetime.datetime.strptime(device.timestamp, "%Y-%m-%d %H:%M:%S.%f")
-
-                if datetime.datetime.now() - datetime.timedelta(seconds = self.timeout) > timestamp:
-                    print("Removing device " + device.deviceID)
-                    self.catalog.database["devices"].pop(device.deviceID)
-
-                    removed = True
-
-            # Check all the services
-            for service in list(self.catalog.database["services"].values()):
-                timestamp = datetime.datetime.strptime(service.timestamp, "%Y-%m-%d %H:%M:%S.%f")
-
-                if datetime.datetime.now() - datetime.timedelta(seconds = self.timeout) > timestamp:
-                    print("Removing service " + service.serviceID)
-                    self.catalog.database["services"].pop(service.serviceID)
-
-                    removed = True
-
-            if removed:
-                # If something's been removed, trigger a sync with the file storage
-                self.catalog.serializeCatalogToJSONFile()
-
-            time.sleep(self.period)
-
-    # To be called when cherrypy stops to stop the thread
-    def stop(self):
-        # cherrypy is stopping -> terminate the thread
-        self.running = False
-
 class RESTCatalog():
     exposed = True
+
+    # Thread class to manage timeouts for RESTCatalog
+    # It is an inner class because it's only purpose is to work together with the Catalog
+    # Also, it is a class and not just a function to pass to threading.Thread because I needed a
+    # class for a cherrypy plugin anyway (to be notified of shutdown)
+    class TimeoutManagerRunner(threading.Thread, cherrypy.process.plugins.SimplePlugin):
+        
+        # timeout is the number of seconds after the last subscription of a service or device to the Catalog such that they must be removed then
+        # period specifies (in seconds, too) the period to check for every device and service
+        def __init__(self, catalog, bus, timeout: float, period: float):
+            threading.Thread.__init__(self)
+            cherrypy.process.plugins.SimplePlugin.__init__(self, bus)
+            
+            self.catalog = catalog
+            self.timeout = timeout
+            self.period = period
+            
+            self.running = True
+
+            # Used to wake the thread up when it's time to shutdown
+            self.wakeEvent = threading.Event()
+
+        def run(self):
+            while self.running:
+                removed = False
+                
+                # Check all the devices
+                for device in list(self.catalog.database["devices"].values()):
+                    timestamp = datetime.datetime.strptime(device.timestamp, "%Y-%m-%d %H:%M:%S.%f")
+
+                    if datetime.datetime.now() - datetime.timedelta(seconds = self.timeout) > timestamp:
+                        print("Removing device " + device.deviceID)
+                        self.catalog.database["devices"].pop(device.deviceID)
+
+                        removed = True
+
+                # Check all the services
+                for service in list(self.catalog.database["services"].values()):
+                    timestamp = datetime.datetime.strptime(service.timestamp, "%Y-%m-%d %H:%M:%S.%f")
+
+                    if datetime.datetime.now() - datetime.timedelta(seconds = self.timeout) > timestamp:
+                        print("Removing service " + service.serviceID)
+                        self.catalog.database["services"].pop(service.serviceID)
+
+                        removed = True
+
+                if removed:
+                    # If something's been removed, trigger a sync with the file storage
+                    self.catalog.serializeCatalogToJSONFile()
+
+                # Wait but be ready to be woken up to shutdown
+                self.wakeEvent.wait(timeout = self.period)
+
+        # To be called when cherrypy stops to stop the thread
+        def stop(self):
+            # cherrypy is stopping -> terminate the thread
+            self.running = False
+
+            # Notify the waiting thread
+            self.wakeEvent.set()
 
     # Used to initialize the attributes
     def __init__(self):
@@ -76,13 +87,18 @@ class RESTCatalog():
         self.database["users"]    = {}
         self.database["services"] = {}
 
+        # Initialize some commonly used jsons for responses
+        self.responseSuccess = { "result": "success" }
+        self.responseSuccessJSON = json.dumps(self.responseSuccess)
+        self.responseFailure = { "result": "failure", "reason": "" }
+
         self.JSONFile = "catalog.json"
 
         # Initialize the json file
         self.serializeCatalogToJSONFile()
 
         # Initialize the thread
-        self.timeoutManagerRunner = TimeoutManagerRunner(self, cherrypy.engine, 120, 60)
+        self.timeoutManagerRunner = RESTCatalog.TimeoutManagerRunner(self, cherrypy.engine, 120, 60)
         self.timeoutManagerRunner.subscribe() # To be notified from cherrypy
         #self.timeoutManagerRunner.start()    # cherrypy calls the start() of its plugins, which happens to be the same method to start threads
 
@@ -92,7 +108,6 @@ class RESTCatalog():
             return obj.serialize()
 
         raise TypeError("Type is not serializable")
-
 
     def serializeCatalogToJSONFile(self):
         # Open the file for switing
@@ -119,19 +134,22 @@ class RESTCatalog():
         # No service has a uri with length other than 1
         if len(uri) != 1:
             cherrypy.response.status = 404 # Not Found
-            return "Not Found"
+            self.responseFailure["reason"] = "Not Found"
+            return json.dumps(self.responseFailure)
 
         if uri[0] == "getMQTTMessageBroker":
             if len(params) != 0:
                 cherrypy.response.status = 404 # Bad Request
-                return "Too many parameters"
+                self.responseFailure["reason"] = "Too many parameters"
+                return json.dumps(self.responseFailure)
                 
             return json.dumps({"url": self.database["MQTTGlobalMessageBrokerURL"], "port": self.database["MQTTGlobalMessageBrokerPort"]})
 
         if uri[0] == "getUser":
             if len(params) != 1 or "id" not in params:
                 cherrypy.response.status = 404 # Bad Request
-                return "Wrong parameters. Usage: getUser?id=<userID>"
+                self.responseFailure["reason"] = "Wrong parameters"
+                return json.dumps(self.responseFailure)
             
             userID = params.get("id")
             if userID in self.database["users"]:
@@ -139,19 +157,22 @@ class RESTCatalog():
 
             # Set the HTTP status
             cherrypy.response.status = 404 # Not Found
-            return "No such user"
+            self.responseFailure["reason"] = "No such user"
+            return json.dumps(self.responseFailure)
            
         if uri[0] == "getUsers":
             if len(params) != 0:
                 cherrypy.response.status = 404 # Bad Request
-                return "Too many parameters"
+                self.responseFailure["reason"] = "Too many parameters"
+                return json.dumps(self.responseFailure)
             
             return json.dumps(list(self.database["users"].values()), default=self.customSerializer)
  
         if uri[0] == "getDevice" and params!={}:
             if len(params) != 1 or "id" not in params:
                 cherrypy.response.status = 404 # Bad Request
-                return "Wrong parameters. Usage: getDevice?id=<deviceID>"
+                self.responseFailure["reason"] = "Wrong parameters"
+                return json.dumps(self.responseFailure)
             
             deviceID = params.get("id")
             if deviceID in self.database["devices"]:
@@ -159,19 +180,22 @@ class RESTCatalog():
 
             # Set the HTTP status
             cherrypy.response.status = 404 # Not Found
-            return "No such device"
+            self.responseFailure["reason"] = "No such device"
+            return json.dumps(self.responseFailure)
            
         if uri[0] == "getDevices":
             if len(params) != 0:
                 cherrypy.response.status = 404 # Bad Request
-                return "Too many parameters"
+                self.responseFailure["reason"] = "Too many parameters"
+                return json.dumps(self.responseFailure)
             
             return json.dumps(list(self.database["devices"].values()), default=self.customSerializer)
        
         if uri[0] == "getService":
-            if len(params) != 1 or id not in params:
+            if len(params) != 1 or "id" not in params:
                 cherrypy.response.status = 404 # Bad Request
-                return "Wrong parameters. Usage: getService?id=<serviceID>"
+                self.responseFailure["reason"] = "Wrong parameters"
+                return json.dumps(self.responseFailure)
             
             serviceID = params.get("id")
             if serviceID in self.database["services"]:
@@ -179,17 +203,20 @@ class RESTCatalog():
             
             # Set the HTTP status
             cherrypy.response.status = 404 # Not Found
-            return "No such service"
+            self.responseFailure["reason"] = "No such service"
+            return json.dumps(self.responseFailure)
            
         if uri[0] == "getServices":
             if len(params) != 0:
                 cherrypy.response.status = 404 # Bad Request
-                return "Too many parameters"
+                self.responseFailure["reason"] = "Too many parameters"
+                return json.dumps(self.responseFailure)
             
             return json.dumps(list(self.database["services"].values()), default=self.customSerializer)
 
         cherrypy.response.status = 404 # Not Found
-        return "Not Found"
+        self.responseFailure["reason"] = "Not Found"
+        return json.dumps(self.responseFailure)
     
     def PUT(self, *uri, **params):
         # Add Service
@@ -199,12 +226,14 @@ class RESTCatalog():
                 body = json.loads(body)
             except:
                 cherrypy.response.status = 400 # Bad Request
-                return "Bad Request: invalid JSON"
+                self.responseFailure["reason"] = "Bad Request: Invalid JSON"
+                return json.dumps(self.responseFailure)
 
             # Check if the json contains everything we need
             if "serviceID" not in body or "description" not in body or "endPoints" not in body:
                 cherrypy.response.status = 400 # Bad Request
-                return "Bad Request"
+                self.responseFailure["reason"] = "Bad Request"
+                return json.dumps(self.responseFailure)
 
             s = Service(body["serviceID"])
             s.description = body["description"]
@@ -216,7 +245,8 @@ class RESTCatalog():
                 except:
                     # Invalid endpoint
                     cherrypy.response.status = 400 # Bad Request
-                    return "Bad Request: invalid end points!"
+                    self.responseFailure["reason"] = "Bad Request: Invalid endpoints"
+                    return json.dumps(self.responseFailure)
 
             # Add the timestamp
             s.timestamp = str(datetime.datetime.now())
@@ -227,7 +257,7 @@ class RESTCatalog():
             # Save the new JSON file
             self.serializeCatalogToJSONFile()
 
-            return "INSERT SERVICE SUCCESS"
+            return self.responseSuccessJSON
 
         # Add User
         if len(uri) == 1 and uri[0] == "addUser":
@@ -236,7 +266,8 @@ class RESTCatalog():
                 body = json.loads(body)
             except:
                 cherrypy.response.status = 400 # Bad Request
-                return "Bad Request: invalid JSON"
+                self.responseFailure["reason"] = "Bad Request: Invalid JSON"
+                return json.dumps(self.responseFailure)
 
             if "userID" not in body or "name" not in body or "surname" not in body or "email" not in body:
                 cherrypy.response.status = 400 # Bad Request
@@ -249,7 +280,7 @@ class RESTCatalog():
             # Save the new JSON file
             self.serializeCatalogToJSONFile()
 
-            return "INSERT USER SUCCESS"
+            return self.responseSuccessJSON
 
         # Add Device
         if len(uri) == 1 and uri[0] == "addDevice":
@@ -258,12 +289,14 @@ class RESTCatalog():
                 body = json.loads(body)
             except:
                 cherrypy.response.status = 400 # Bad Request
-                return "Bad Request: invalid JSON"
+                self.responseFailure["reason"] = "Bad Request: Invalid JSON"
+                return json.dumps(self.responseFailure)
  
             # Check if the json contains everything we need
             if "deviceID" not in body or "endPoints" not in body or "availableResources" not in body:
                 cherrypy.response.status = 400 # Bad Request
-                return "Bad Request"
+                self.responseFailure["reason"] = "Bad Request"
+                return json.dumps(self.responseFailure)
  
             d = Device(body["deviceID"], body["availableResources"])
  
@@ -274,7 +307,8 @@ class RESTCatalog():
                 except:
                     # Invalid endpoint
                     cherrypy.response.status = 400 # Bad Request
-                    return "Bad Request: invalid end points!"
+                    self.responseFailure["reason"] = "Bad Request: Invalid endpoints"
+                    return json.dumps(self.responseFailure)
 
             # Add timestamp
             d.timestamp = str(datetime.datetime.now())
@@ -285,11 +319,12 @@ class RESTCatalog():
             # Save the new JSON file
             self.serializeCatalogToJSONFile()
  
-            return "INSERT DEVICE SUCCESS"
+            return self.responseSuccessJSON
                 
         # The requested service does not exist
         cherrypy.response.status = 404 # Not Found
-        return "Not Found"
+        self.responseFailure["reason"] = "Not Found"
+        return json.dumps(self.responseFailure)
 
     # def POST(self, *uri, **params):
     #     # The POST method is only used for setting the global message broker url and port
---
+++ SW/part2/es04.py
@@ -0,0 +1,146 @@
+import cherrypy
+import json
+import conversions
+import requests
+import threading
+
+class RESTWebService():
+    exposed = True
+
+    # Inner Thread class for continuous subscription with the Service Catalog
+    class ServiceCatalogSubscriberRunner(threading.Thread, cherrypy.process.plugins.SimplePlugin):
+
+        # subscriptionPayload is the json-formatted string to send the catalog every 'period' seconds
+        def __init__(self, bus, period: float, subscriptionPayload: str, catalogURL: str):
+            threading.Thread.__init__(self)
+            cherrypy.process.plugins.SimplePlugin.__init__(self, bus)
+
+            self.period = period
+            self.subscriptionPayload = subscriptionPayload
+            self.catalogURL = catalogURL
+            
+            self.running = True
+            self.wakeEvent = threading.Event()
+
+        def run(self):
+            while self.running:
+                # Perform the subscription
+                print("Registering to the catalog...")
+                
+                try:
+                    response = requests.put(self.catalogURL, data = self.subscriptionPayload, timeout = 1.5)
+                except (ConnectionError, requests.HTTPError, requests.Timeout):
+                    print("An error occurred while performing the request to the service catalog.")
+                else:
+                    print("Registration successful, got " + response.text)
+
+                self.wakeEvent.wait(self.period)
+
+        def stop(self):
+            # We have been notified from cherrypy that the server is shutting down
+            # Stop the thread
+            self.running = False
+            self.wakeEvent.set()
+    
+    validUnits = ["C", "K", "F"]
+    listValue = []
+
+    def __init__(self):
+        # Register the cherrypy plugin (which is also the thread that must register this service to the catalog)
+        self.registerPayload = json.dumps(
+            {
+                "serviceID": "temperatureServiceV1",
+                "description": "A temperature converter between C, F and K. Pass the temperatures as HTTP GET parameters: /converter?value=<original_value>&originalUnit=<original_unit>&targetUnit=<target_unit>. \
+                    Additionally, get the current log of temperatures recorder by the edge sensor via HTTP GET at /log, or add to the log via HTTP POST at /log",
+                "endPoints": [{"service": "http://localhost:8081/converter", "type": "webService"},
+                {"service": "http://localhost:8081/log", "type": "webService"}]
+            }
+        )
+        self.catalogSubscriber = RESTWebService.ServiceCatalogSubscriberRunner(cherrypy.engine, 5, self.registerPayload, "http://localhost:8080/addService")
+        self.catalogSubscriber.subscribe() # This also starts the thread
+
+    def buildJSON(self, originalValue: float, originalUnit: str, targetValue: float, targetUnit: str) -> str:
+        retJSON = {"originalValue": originalValue, "originalUnit": originalUnit, "targetValue": targetValue, "targetUnit": targetUnit}
+        return json.dumps(retJSON)
+
+    def GET(self, *uri, **params):
+        if len(uri) != 1:
+            # Whathever the uri was, it's not valid
+            # Set the appropriate HTTP status
+            cherrypy.response.status = 404 # Not Found
+            return "Command not supported!"
+
+        # Shutdown on "shutdown". Used for debugging purposes
+        if uri[0] == "shutdown":
+            cherrypy.engine.exit()
+            return ""
+
+        if uri[0] == "converter":
+            # Check if the parameters are valid
+            if len(params) != 3 or "value" not in params or "originalUnit" not in params or "targetUnit" not in params:
+                cherrypy.response.status = 400 # Bad Request
+                return "Wrong parameters!<br>Usage: converter?value=&lt;val&gt;&originalUnit=&lt;orig&gt;&targetUnit=&lt;trgt&gt;."
+
+            originalUnit = params["originalUnit"].upper()
+            targetUnit = params["targetUnit"].upper()
+
+            # Check if the units are valid
+            if originalUnit not in self.validUnits or targetUnit not in self.validUnits:
+                cherrypy.response.status = 400 # Bad Request
+                return "Invalid temperature unit!<br>Valid units are:<ul><li>C - Celsius</li><li>K - Kelvin</li><li>F - Fahrenheit</li></ul>"
+
+            try:
+                originalValue = float(params["value"])
+            except:
+                # The numerical value is invalid!
+                cherrypy.response.status = 400 # Bad Request
+                return "Invalid temperature value!"
+
+            # Perform the conversion
+            try:
+                targetValue = conversions.convert(originalValue, originalUnit, targetUnit)
+            except ValueError as e:
+                cherrypy.response.status = 400 # Bad Request
+                return str(e)
+
+            return self.buildJSON(originalValue, originalUnit, targetValue, targetUnit)
+        
+        if uri[0] == "log":
+            # Log all the temperature data received so far
+            return json.dumps(self.listValue) # Use json.dumps to print the list instead of just converting
+                                              # it into a string to print with the double quotes to be compliant with JSON
+
+        # The command is not valid
+        cherrypy.response.status = 404 # Not Found
+        return "Command not supported!"
+    
+    def POST(self, *uri, **params):
+        # Check if the uri is valid
+        if len(uri) == 1 and uri[0] == "log":
+            body = cherrypy.request.body.read();
+            val = json.loads(body);
+
+            # Check if the received JSON is correct
+            if ("bn" not in val or val["bn"] != "Yun" or "e" not in val
+                    or "n" not in val["e"][0] or val ["e"][0]["n"] != "temperature"
+                    or "t" not in val["e"][0] or "v" not in val["e"][0] or "u" not in val["e"][0]):
+                # The body is not what we expected -> reject it
+                cherrypy.response.status = 400 # Bad Request
+                return
+
+            self.listValue.append(val)
+
+if __name__ == "__main__":
+    conf = {
+        "/": {
+            "request.dispatch": cherrypy.dispatch.MethodDispatcher(),
+            "tools.sessions.on": True
+        }
+    }
+
+    cherrypy.tree.mount(RESTWebService(), "/", conf)
+    cherrypy.config.update({"server.socket_host": "0.0.0.0"})
+    cherrypy.config.update({'server.socket_port': 8081}) # Change the port to run it simultaneously with the catalog on the same machine
+
+    cherrypy.engine.start()
+    cherrypy.engine.block()

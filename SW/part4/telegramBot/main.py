97676a9b7a3126f582b95cd6d173296a87728b8f
Davide Miola <davide.miola99@gmail.com>
2020-07-15 18:52:53 +0200

    Added basic end point management

--- SW/part4/telegramBot/main.py
+++ SW/part4/telegramBot/main.py
@@ -7,6 +7,29 @@ import telegram.ext as ext
 import telegram
 import matplotlib.pyplot as plt
 
+# Takes an end point as a python dict and produces its string representation for displaying as a telegram menu button.
+# Assumes the end point is valid
+def endPointToMenuButton(endPoint) -> str:
+    res = ""
+
+    if endPoint["type"] == "webService":
+        res += "HTTP"
+        
+        if endPoint["webType"] == "producer":
+            res += " prod: "
+        else:
+            res += " cons: "
+        
+    else:
+        res += "MQTT"
+
+        if endPoint["mqttClientType"] == "subscriber":
+            res += "sub: "
+        else:
+            res += "pub: "
+    
+    return res + endPoint["service"]
+
 class TelegramBot:
     def __init__(self, catalogHOST: str, catalogPORT: int, botToken: str):
         print("LOG: Telegram Bot initialization BEGIN")
@@ -51,6 +74,7 @@ class TelegramBot:
         self.botUpdater = ext.Updater(token = self.telegramBotToken, use_context = True)
         self.botDispatcher = self.botUpdater.dispatcher
         self.botDispatcher.add_handler(ext.CommandHandler(command = "start", callback = self.botClbStart))
+        self.botDispatcher.add_handler(ext.CallbackQueryHandler(self.botClbQueryHandler))
 
         print("LOG: Telegram Bot initialization END")
 
@@ -82,7 +106,12 @@ class TelegramBot:
     
     def retrieveRegistrations(self):
         # This piece of code will be executed by two distinct threads -> it must be synchronized
-        self.retrieveRegistrationsLock.acquire(blocking = True)
+        # (if another thread is already executing this code, just wait for its completion and not do it again)
+        if not self.retrieveRegistrationsLock.acquire(blocking = False):
+            # Wait until the owner of the lock is done and then return
+            self.retrieveRegistrationsLock.acquire(blocking = True)
+            self.retrieveRegistrationsLock.release()
+            return
 
         try:
             response = requests.get(self.catalogEndPoint + "/getDevices")
@@ -130,12 +159,107 @@ class TelegramBot:
         if not self.catalogAvailable:
             message = "Hi, currently the catalog is not available"
         else:
-            self.retrieveRegistrations()
-            message = "Hi, currently connected to the catalog at " + self.catalogEndPoint + \
-                      ".\n\nAvailable devices: " + str(self.availableDevices) + \
-                      "\n\nAvailable services: " + str(self.availableServices)
+            message = "Hi, currently connected to the catalog at " + self.catalogEndPoint
         
         context.bot.send_message(chat_id = update.effective_chat.id, text = message)
+
+        if self.catalogAvailable:
+            self.botMainMenu(update, context)
+
+    def botMainMenu(self, update: telegram.Update, context: ext.CallbackContext):
+        # Send the list of available devices and services as a button menu
+        self.retrieveRegistrations()
+        message = "Choose one of the available devices or services"
+        context.bot.send_message(chat_id = update.effective_chat.id, text = message)
+
+        # Build the menu
+        # Services
+        if len(self.availableServices) > 0:
+            menu = []
+            for s in self.availableServices.values():
+                menu.append([telegram.InlineKeyboardButton(s["serviceID"], callback_data = "s_" + s["serviceID"])])
+            
+            context.bot.send_message(chat_id = update.effective_chat.id, text = "Services:", reply_markup = telegram.InlineKeyboardMarkup(menu))
+        else:
+            context.bot.send_message(chat_id = update.effective_chat.id, text = "No registered service")
+
+        # Devices
+        if len(self.availableDevices) > 0:
+            menu = []
+            for d in self.availableDevices.values():
+                menu.append([telegram.InlineKeyboardButton(d["deviceID"], callback_data = "d_" + d["deviceID"])])
+            
+            context.bot.send_message(chat_id = update.effective_chat.id, text = "Devices:", reply_markup = telegram.InlineKeyboardMarkup(menu))
+        else:
+            context.bot.send_message(chat_id = update.effective_chat.id, text = "No registered device")
+    
+    def botClbQueryHandler(self, update: telegram.Update, context: ext.CallbackContext):
+        data = update.callback_query.data
+        
+        # Discriminate between devices and services
+        if data[0:1] == "s":
+            # Service
+            # Check if the service is still valid (could be an outdated request)
+            print("Checking '" + data[2:] + "'")
+            if data[2:] not in self.availableServices:
+                context.bot.send_message(chat_id = update.effective_chat.id, text = "The selected service is no longer available")
+                self.botMainMenu(update, context)
+            
+            self.botHandleServiceRequest(update, context, data[2:])
+
+        elif data[0:1] == "d":
+            # Device
+            print("Checking '" + data[2:] + "'")
+            if data[2:] not in self.availableDevices:
+                context.bot.send_message(chat_id = update.effective_chat.id, text = "The selected device is no longer available")
+                self.botMainMenu(update, context)
+
+            self.botHandleDeviceRequest(update, context, data[2:])
+        
+        elif data[0:1] == "m":
+            # Go to main menu
+            self.botMainMenu(update, context)
+
+        # Else it's an invalid request, ignore it
+    
+    def botHandleServiceRequest(self, update: telegram.Update, context: ext.CallbackContext, serviceID: str):
+        print("Handling service request for " + serviceID)
+
+        message = serviceID + "\n\nDescription:\n" + self.availableServices[serviceID]["description"]
+        context.bot.send_message(chat_id = update.effective_chat.id, text = message)
+
+        # Produce list of end points
+        menu = []
+        for (i, e) in enumerate(self.availableServices[serviceID]["endPoints"]):
+            # Telegram API has a nasty 64 byte limitation on the callback_data, so I had to get creative
+            menu.append([telegram.InlineKeyboardButton(endPointToMenuButton(e), callback_data = "e_s_" + serviceID + "_" + str(i))])
+        
+        # Append 'go back' button
+        menu.append([telegram.InlineKeyboardButton("Go Back", callback_data = "m")])
+
+        if len(menu) == 1:
+            context.bot.send_message(chat_id = update.effective_chat.id, text = "No end point available", reply_markup = telegram.InlineKeyboardMarkup(menu))
+        else:
+            context.bot.send_message(chat_id = update.effective_chat.id, text = "Available end points:", reply_markup = telegram.InlineKeyboardMarkup(menu))
+
+    def botHandleDeviceRequest(self, update: telegram.Update, context: ext.CallbackContext, deviceID: str):
+        print("Handling device request for " + deviceID)
+
+        message = deviceID + "\n\nResources:\n" + str(self.availableDevices[deviceID]["resources"])
+        context.bot.send_message(chat_id = update.effective_chat.id, text = message)
+
+        # Produce list of end points
+        menu = []
+        for (i, e) in enumerate(self.availableDevices[deviceID]["endPoints"]):
+            menu.append([telegram.InlineKeyboardButton(endPointToMenuButton(e), callback_data = "e_d_" + deviceID + str(i))])
+        
+        # Append 'go back' button
+        menu.append([telegram.InlineKeyboardButton("Go Back", callback_data = "m")])
+
+        if len(menu) == 1:
+            context.bot.send_message(chat_id = update.effective_chat.id, text = "No end point available", reply_markup = telegram.InlineKeyboardMarkup(menu))
+        else:
+            context.bot.send_message(chat_id = update.effective_chat.id, text = "Available end points:", reply_markup = telegram.InlineKeyboardMarkup(menu))
 
 if __name__ == "__main__":
     bot = TelegramBot("http://localhost", 8080, "1229244529:AAGhUR_oqvcp-nToD4yPwhOk7NM_o57qbLQ")
